dbfc887455dfbb92db5f7650b8cea59e36bbc07b
Daniel Posada_PUJ <danielp0sada@javerianacali.edu.co>
2025-02-04 10:46:25 -0500

    Update Archivo 1 Librería
    Se modificó el archivo para adecuarse a la versión descrita en la actividad.

--- seccion_1/MMAF.py
+++ seccion_1/MMAF.py
@@ -25,11 +25,10 @@ def calcular_poblacion(juveniles_iniciales, adultas_iniciales, tasa_reproduccion
         # Cálculos
         juveniles_nuevas = adultas * tasa_reproduccion
         juveniles_que_pasan = juveniles * tasa_conversion
-        juveniles_que_permanecen = juveniles * (1 - tasa_conversion)
         adultas_sobrevivientes = adultas * tasa_supervivencia
 
         # Nuevos valores de juveniles y adultas
-        juveniles = juveniles_nuevas + juveniles_que_permanecen
+        juveniles = juveniles_nuevas
         adultas = juveniles_que_pasan + adultas_sobrevivientes
 
         # Población total
